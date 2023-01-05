ea1f3a3850fc7c7edd70fef53b9b5761430b81cc
Iceonq <bartek.sosnowicz@gmail.com>
2023-01-05 11:24:07 +0100

    1+2 hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const link = document.createElement('a');
+  const img = document.createElement('img');
+  gallery.append(link);
+  link.append(img);
+  img.classList.add('gallery__image');
+  link.classList.add('gallery__item');
+  link.setAttribute('href', `${galleryItems[i].original}`);
+  img.setAttribute('src', `${galleryItems[i].preview}`);
+  img.setAttribute('alt', `${galleryItems[i].description}`);
+  img.dataset.source = `${galleryItems[i].original}`;
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const onTimeUpdater = ({ seconds } = 0) => {
+  localStorage.setItem(STORAGE_KEY, seconds);
+  console.log(seconds);
+};
+
+const getVideoCurrentTime = () => {
+  return localStorage.getItem(STORAGE_KEY) || 0;
+};
+
+player.setCurrentTime(getVideoCurrentTime()).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      alert('error');
+      break;
+
+    default:
+      break;
+  }
+});
+
+player.on('timeupdate', throttle(onTimeUpdater, 1000));
